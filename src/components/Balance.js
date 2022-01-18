3c0b9b2a92059d6a3ec114e6f344549660c7435d
Adrian Werimo <adrwer7@gmail.com>
2022-01-18 14:01:30 +0300

    Element Id cleanup

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ export const Balance = () => {
     return (
         <div>
             <h4>Balance</h4>
-            <h1 id='balance'>Kshs. 0.00</h1>
+            <h1>Kshs. 0.00</h1>
         </div>
     )
 }
