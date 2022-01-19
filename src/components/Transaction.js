07f1153b9d01e40ae42fde05695f136696c80f12
Adrian Werimo <adrwer7@gmail.com>
2022-01-19 11:21:58 +0300

    added actions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({transaction}) => {
+    const { deleteTransaction } = useContext(GlobalContext)
+
     const sign = transaction.amount < 0 ? '-':'+'
     const borderColor = transaction.amount < 0 ? 'minus':'plus'
 
     return (
         <li className={borderColor}>
-            {transaction.text} <span>{sign} Kshs. {Math.abs(transaction.amount)}</span><button className='delete-btn'>X</button>
+            {transaction.text} <span>{sign} Kshs. {Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className='delete-btn'>X</button>
         </li>
     )
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-    transactions: [
-        {id: '1', text: 'Flowers', amount: -500 },
-        {id: '2', text: 'Salary', amount: 100000 },
-        {id: '3', text: 'Book', amount: -3000 },
-        {id: '4', text: 'Laptop', amount: 120000 },
-    ]
+    transactions: []
 }
 
 // create context
@@ -17,7 +12,22 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState)
 
-    return(<GlobalContext.Provider value={{transactions:state.transactions}}>
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+    return(<GlobalContext.Provider value={{transactions:state.transactions, deleteTransaction, addTransaction}}>
         {children}
     </GlobalContext.Provider>)
 }
